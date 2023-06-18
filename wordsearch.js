2510e0041f80286e2458393871f7d980e0f2e414
RonnieGuo <guorongfly@gmail.com>
2023-06-18 16:28:32 -0600

    added comment

--- wordsearch.js
+++ wordsearch.js
@@ -2,11 +2,13 @@ const wordSearch = (letters, word) => {
     if (word === '') {
         return false; // If the word is empty, return false
       }
+
+    // If the word is found horizontally, return true
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
-    
+    // If the word is found vertically, return true
     for (let row of letters) {
         const columnJoin = row.join('');
         if (columnJoin.includes(word)) {
