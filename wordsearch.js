ea8839d7ebc5b78532d63b4856a2421d72892aa4
RonnieGuo <guorongfly@gmail.com>
2023-06-18 16:23:05 -0600

    editted

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,20 @@
 const wordSearch = (letters, word) => { 
+    if (word === '') {
+        return false; // If the word is empty, return false
+      }
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
-}
+    
+    for (let row of letters) {
+        const columnJoin = row.join('');
+        if (columnJoin.includes(word)) {
+          return true; 
+        }
+      }
+    
+      return false; 
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
